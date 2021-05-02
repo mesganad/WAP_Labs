eb106c9850d761a5c3b1edf06a5744481cdfc52f
misgan asmamaw <hello@misgancode>
2021-05-02 00:55:47 -0500

    Lab8 update

--- JavaScriptLabs/Lab8/Question2.js
+++ JavaScriptLabs/Lab8/Question2.js
@@ -17,6 +17,23 @@ Student.prototype.display = function () {
     return 'the Average grade for ' + this.firstName + " " + this.lastName + " " + this.computeAverageGrade();
 }
 
+Array.prototype.mysort = function () {
+    let arr = this;
+    let len = arr.length;
+    for (let i = len - 1; i >= 0; i--) {
+      for (let j = 1; j <= i; j++) {
+        if (arr[j - 1] > arr[j]) {
+          let temp = arr[j - 1];
+          arr[j - 1] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+    return arr;
+  }
+  
+ 
+
 let students = [
     new Student('JOhn', 'chris', [2, 3]),
     new Student('abel', 'charless', [3, 2]),
@@ -24,7 +41,8 @@ let students = [
 ];
 let result = students.map(a => a.computeAverageGrade()).reduce((accum, elem) => accum + elem / students.length, 0);
 console.log('Total grade average: '+result);
-let sorted=students.map(a=>a.computeAverageGrade()).sort();
-console.log('Sorted by average salary: '+sorted);
+
+ console.log([7, 5, 2, 4, 3, 9].mysort());
+
 
 
